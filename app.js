08c05fc15b83b7b6d2908a793ae3cd75c53fdfd4
Charan Prajwal <charanprajwal@Charans-MacBook-Pro.local>
2023-07-19 14:09:25 +0530

    added

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(express.json())
 app.get('/', (req, res) => {
-  res.send('hello worlds' )
+  res.send('hello worl' )
  })
 app.get("/retreivenotes", async (req, res) => {
   const notes = await getNotes()
