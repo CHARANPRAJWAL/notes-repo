b770362ad46b80027b6f277b55f4fd570021f0f2
charan <charanprajwal@Charans-MacBook-Pro.local>
2023-11-04 22:17:55 +0530

    changed display name again

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(express.json())
 app.get('/', (req, res) => {
-  res.send('Hello CI/CD!!')
+  res.send('Hii world!!')
  })
 app.get("/retreivenotes", async (req, res) => {
   const notes = await getNotes()
