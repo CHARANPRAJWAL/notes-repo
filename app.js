505943d9c240513ff4acbef67a2e60ea1c438fda
charan <charanprajwal@Charans-MacBook-Pro.local>
2023-11-04 22:37:32 +0530

    changed display

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(express.json())
 app.get('/', (req, res) => {
-  res.send('Hello code build CI/CD!!')
+  res.send('Hello CI/CD!!')
  })
 app.get("/retreivenotes", async (req, res) => {
   const notes = await getNotes()
